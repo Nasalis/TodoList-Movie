8766d653f089eda8c187d02c006b3d3927527020
Nasalis <davidossantosfreitas@hotmail.com>
2021-03-25 17:46:55 -0300

    Alert messages added

--- src/components/AddItem/index.tsx
+++ src/components/AddItem/index.tsx
@@ -9,13 +9,19 @@ export default function AddItem() {
 
     const { addNewTodo } = useContext(TableContext)
 
+    const [activeAlert, setActiveAlert] = useState(false)
+    const [errorAlert, setErrorAlert] = useState(false);
+
+
     const [name, setName] = useState("");
     const [director, setDirector] = useState("");
     const [runtime, setRuntime] = useState(0);
 
+
     function addNewMovie() {
         if(name === "" || director === "" || runtime === 0) {
-            console.log("DADOS INVÁLIDOS")
+            setErrorAlert(true);
+            setActiveAlert(false);
             return;
         }
         let newMovie: Todo;
@@ -27,6 +33,8 @@ export default function AddItem() {
             completed: false,
         }
         addNewTodo(newMovie);
+        setActiveAlert(true);
+        setErrorAlert(false);
         
     }
 
@@ -39,18 +47,24 @@ export default function AddItem() {
                 <form action="" method="POST">
                     <label htmlFor="">
                         <h4>Nome</h4>
-                        <input type="text" onChange={input => setName(input.target.value)}/>
+                        <input type="text" onChange={input => setName(input.target.value)} required/>
                     </label>
                     <label htmlFor="">
                         <h4>Diretor</h4>
-                        <input type="text" onChange={input => setDirector(input.target.value)}/>
+                        <input type="text" onChange={input => setDirector(input.target.value)} required/>
                     </label>
                     <label htmlFor="">
                         <h4>Duração (minutos)</h4>
-                        <input type="number" onChange={input => setRuntime(Number(input.target.value))}/>
+                        <input type="number" onChange={input => setRuntime(Number(input.target.value))} required/>
                     </label>
                     <button type="button" onClick={addNewMovie}>Adicionar</button>   
                 </form>
+            </div>
+            <div className={`formAlert ${activeAlert ? 'active' : ""}`}>
+                <h1>Item adicionado</h1>
+            </div>
+            <div className={`formAlert ${errorAlert ? 'error' : ""}`}>
+                <h1>Erro ao adicionar</h1>
             </div>
         </>
     )
